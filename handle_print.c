027d40c21a5d2d33033a5ecba7e87c0e095b883d
Akorfanusi <128804067+Akorfanusi@users.noreply.github.com>
2023-05-28 23:25:54 +0100

    handle_print.c1

--- handle_print.c
+++ handle_print.c
@@ -16,11 +16,11 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
-		{'c', print_char}, {'s', print_string}, {'%', print_percent},
-		{'i', print_int}, {'d', print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+		{'c', print_char}, {'s', print_string}, {'%', percent_sign},
+		{'i', print_integer}, {'d', print_integer}, {'b', unsgnd_binary},
+		{'u', print_unsigned_num}, {'o', print_octal_notation}, {'x', hexadecimal_notation},
+		{'X', hexa_upper_notation}, {'p', print_pointer}, {'S', _non_printable},
+		{'r', print_reverse}, {'R', string_rot13}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
